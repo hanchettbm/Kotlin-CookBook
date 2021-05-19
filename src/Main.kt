55cf768d115b78cb17f862123b262e7b862679c4
hanchettbm <hanchettbm@gmail.com>
2021-05-19 10:00:03 -0600

    Commit

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,35 @@
+import java.util.Scanner
+
 fun main() {
-    println("Hello World")
+    var leave = false
+    while (!leave) {
+        val scanner = Scanner(System.`in`)
+        println("Welcome to your CookBook! What would you like to do?")
+        println("A) Add a recipe B) Remove a recipe C) View a recipe D) View CookBook Q) Quit")
+        print("> ")
+        val input : String = scanner.next()
+        if (input == "A") {
+            println ("Testing A!")
+//            leave = true
+        }
+        if (input == "B") {
+            println ("Testing B")
+//            leave = true
+        }
+        if (input == "C") {
+            println ("Testing C")
+//            leave = true
+        }
+        if (input == "D") {
+            println ("Testing D")
+//            leave = true
+        }
+        if (input == "Q") {
+            println ("Goodbye!")
+            leave = true
+        }
+
+
+    }
+
 }
